0e5fdd7ceff93a2c610b1556e5fa776573b0af62
James Robert Lloyd <jrl44@cam.ac.uk>
2013-05-24 15:05:27 +0100

    Updated figures and included plotting code

---
+++ source/gp/figures.m
@@ -0,0 +1,277 @@
+%% Code used for producing figures
+
+%% Import load and temperature data
+
+data = importdata('load_input.csv',',');
+times = data(:,1);
+loads = data(:,2:end);
+data = importdata('GP_pred_temp.csv',',');
+temps = data(:,1:end);
+data = load('smooth_temp_GP.mat', 'smoothed');
+smooth_temps = data.smoothed;
+clear data
+
+%% Split into test and train
+
+train = (loads(:,1) ~= 0);
+test  = (loads(:,1) == 0);
+
+%% For plotting
+
+date_offset = 695421;
+
+%% Plot zone 10
+
+h = figure;
+hold on;
+train_times = times(train);
+train_loads = loads(train,10);
+plot (train_times((end-5000):(end-3700)) + date_offset, train_loads((end-5000):(end-3700)), '-', 'LineWidth', 1);
+xlabel('Time');
+ylabel('Load (Z10)');
+%set(gca,'XTick',365)
+datetick('x', 'mmm yyyy');
+hold off;
+getframe;  
+save2pdf ('zone_10.pdf', h, 600);
+
+%% Plot zone 9
+
+h = figure;
+hold on;
+train_times = times(train);
+train_loads = loads(train,9);
+plot (train_times((end-4000):(end-3600)) + date_offset, train_loads((end-4000):(end-3600)), '-', 'LineWidth', 2);
+xlabel('Time');
+ylabel('Load (Z09)');
+%set(gca,'XTick',365)
+datetick('x', 'dd mmm yyyy');
+hold off;
+getframe;  
+save2pdf ('zone_09.pdf', h, 600);
+
+%% Remove means
+
+means = mean(loads(train,:));
+loads = loads - repmat(means, size(loads, 1), 1);
+
+means_t = mean(temps(train,:));
+temps = temps - repmat(means_t, size(temps, 1), 1);
+
+means_st = mean(smooth_temps(train,:));
+smooth_temps = smooth_temps - repmat(means_st, size(smooth_temps, 1), 1);
+
+%% Scale the data
+
+sds = std(loads(train,:));
+loads = loads ./ repmat(sds, size(loads, 1), 1);
+
+sds_t = std(temps(train,:));
+temps = temps ./ repmat(sds_t, size(temps, 1), 1);
+
+sds_st = std(smooth_temps(train,:));
+smooth_temps = smooth_temps ./ repmat(sds_st, size(smooth_temps, 1), 1);
+
+%% Plot zone 1 and temperature 9
+
+h = figure;
+hold on;
+train_times = times(train);
+train_loads = loads(train,1);
+train_temps = temps(train,9);
+[AX,H1,H2] = plotyy(train_times(1:250) + date_offset, train_loads(1:250), train_times(1:250) + date_offset, train_temps(1:250)); 
+set(get(AX(1),'Ylabel'),'String','Load (Z01 standardised)'); 
+set(get(AX(2),'Ylabel'),'String','Temperature (T09 standardised)');
+set(H1,'LineStyle','-');
+set(H2,'LineStyle','--');
+set(H1,'LineWidth',2);
+set(H2,'LineWidth',2);
+xlabel('Time');
+datetick(AX(1), 'x', 'dd mmm yyyy', 'keepticks');
+datetick(AX(2), 'x', 'dd mmm yyyy', 'keepticks');
+%set(AX(2), 'XTick',[])
+hold off;
+save2pdf ('gef_load_z01_t09_250.pdf', h, 600);
+
+%% Main prediction code
+
+for i = 1:1
+  % Find the prediction regions in order
+  searching_for_start = true;
+  for j = 1:length(times)
+    if searching_for_start
+      if test(j)
+        start_index = j-1;
+        k = j + 1;
+        searching_for_start = false;
+        searching_for_end = true;
+        while searching_for_end
+          if (k > length(times)) || train(k) 
+            searching_for_end = false;
+            end_index = k;
+          else
+            k = k+1;
+          end
+        end
+        % Found a region - try different models and combine to form
+        % prediction
+        lmls = zeros(size(temps, 2), 1);
+        for t_i = 1:size(temps, 2)
+          % Setup training and testing data splits
+          if k < length(times)
+            forecast = false;
+            window = 500;
+            train_times = [times((start_index - window):(start_index)) ; ...
+                           times((end_index):(end_index + window))];
+            train_loads = [loads((start_index - window):(start_index), i) ; ...
+                           loads((end_index):(end_index + window), i)];
+            train_temps = [temps((start_index - window):(start_index), t_i) ; ...
+                           temps((end_index):(end_index + window), t_i)];
+            train_smooth_temps = [smooth_temps((start_index - window):(start_index), t_i) ; ...
+                           smooth_temps((end_index):(end_index + window), t_i)];
+            test_times  = times((start_index+1):(end_index-1));
+            test_temps  = temps((start_index+1):(end_index-1), t_i);
+            test_smooth_temps  = smooth_temps((start_index+1):(end_index-1), t_i);
+            test_indices = (start_index+1):(end_index-1);
+            all_times = [times((start_index - window):(end_index + window))];
+            all_temps = [temps((start_index - window):(end_index + window),:)];
+          else
+            forecast = true;
+            window = 500;
+            train_times = times((start_index - window):(start_index));
+            train_loads = loads((start_index - window):(start_index), i);
+            train_temps = temps((start_index - window):(start_index), t_i);
+            train_smooth_temps = smooth_temps((start_index - window):(start_index), t_i);
+            test_times  = times((start_index+1):(end_index-1));
+            test_temps  = temps((start_index+1):(end_index-1), t_i);
+            test_smooth_temps  = smooth_temps((start_index+1):(end_index-1), t_i);
+            test_indices = (start_index+1):(end_index-1);
+            all_times = [times((start_index - window):(end_index-1))];
+            all_temps = [temps((start_index - window):(end_index-1), :)];
+          end
+          % Now do GP regression
+          x = [train_times train_smooth_temps train_temps];
+          x_test = [test_times test_smooth_temps test_temps];
+          y = train_loads;
+          
+          if forecast
+            if i == 9
+              % This time series looks mostly random - use a simpler kernel
+              x = x(:,1);
+              x_test = x_test(:,1);
+              covfunc = {@covSum, {@covSEiso, @covSEiso, @covPeriodic}};
+              hyp.cov = [log(3) ; log(0.3) ; 
+                         log(10) ; log(0.5) ; 
+                         log(0.1) ; log(1) ; log(1)];
+            else
+              covfunc = @covElec01;
+              % Equivalently - this kernel can be written in GPML
+              % {@covSum, {{@covMask, {[1, 0, 0], @covSEiso}}, {@covMask, {[0, 1, 0], @covSEiso}},
+              %            {@covProd, {{@covMask, {[0, 0, 1], @covSEiso}},
+              %            {@covMask, {[1, 0, 0], @covPeriodic}}}}}
+              hyp.cov = [log(2) ; log(0.3) ; 
+                         log(0.5) ; log(0.5) ;
+                         log(0.5) ; log(0.5) ;
+                         log(0.1) ; log(1) ; log(1)];
+            end
+          else
+            if i == 9
+              % This time series looks mostly random - use a simpler kernel
+              x = x(:,1);
+              x_test = x_test(:,1);
+              covfunc = {@covSum, {@covSEiso, @covSEiso, @covPeriodic}};
+              hyp.cov = [log(3) ; log(0.3) ; 
+                         log(10) ; log(0.5) ; 
+                         log(0.1) ; log(1) ; log(1)];
+            else
+              covfunc = @covElec02;
+              % Equivalently - this kernel can be written in GPML
+              % {@covSum, {{@covMask, {[1, 0, 0], @covSEiso}}, {@covMask, {[0, 1, 0], @covSEiso}},
+              %            {@covProd, {{@covMask, {[0, 0, 1], @covSEiso}},
+              %                        {{@covMask, {[1, 0, 0], @covSEiso}}
+              %                        {@covMask, {[1, 0, 0], @covPeriodic}}}}}
+              hyp.cov = [log(2) ; log(0.3) ; 
+                         log(0.5) ; log(0.5) ;
+                         log(0.5) ; log(0.5) ;
+                         log(2) ; log(0.5) ; 
+                         log(0.1) ; log(1) ; log(1)];
+            end
+          end
+          likfunc = @likGauss;
+          if i == 9
+            hyp.lik = log(0.8);
+          else
+            hyp.lik = log(0.2);
+          end
+          meanfunc = @meanZero;
+          hyp.mean = [];
+          hyp_opt = minimize(hyp, @gp, -1, @infExact, meanfunc, covfunc, likfunc, ...
+                             x, y);
+                           
+          [m, ~] = gp(hyp_opt, @infExact, meanfunc, covfunc, likfunc, x, y, [x_test ; x]);
+
+          if t_i == 1
+            predictions = zeros(length(m), size(temps, 2));
+          end
+          predictions(:, t_i) = m(:);
+          lmls(t_i) = -gp(hyp_opt, @infExact, meanfunc, covfunc, likfunc, x, y);
+
+        end
+        % Average predictions, plot and save
+        weights = exp(lmls - max(lmls)) / sum(exp(lmls - max(lmls)));
+        av_prediction = predictions * weights;
+        
+        % Plot
+        
+        best_i = find(weights == max(weights));
+        h = figure;
+        plot (train_times(1:(window+1)) + date_offset, train_loads(1:(window+1)), 'k-', 'LineWidth', 2);
+%         [AX,H1,H2] = plotyy(train_times, train_loads, all_times, all_temps(:,best_i), 'plot');
+        hold on
+%         plot (train_times, av_prediction((length(test_times)+1):end), 'g');
+        if ~forecast
+          plot (train_times((window+2):end) + date_offset, train_loads((window+2):end), 'k-', 'LineWidth', 2);
+        end
+        plot (test_times + date_offset, av_prediction(1:length(test_times)), 'r--', 'LineWidth', 2);
+        plot ([min(test_times) + date_offset, min(test_times) + date_offset], [-1, 3], 'k--');
+        plot ([max(test_times) + date_offset, max(test_times) + date_offset], [-1, 3], 'k--');
+        xlim ([times(start_index+1)-5 + date_offset, times(end_index-1)+5 + date_offset]);
+        ylim ([-1, 3]);
+        xlabel('Time')
+        ylabel('Load (Z01 standardised)')
+        datetick('x', 'dd mmm yyyy', 'keeplimits');
+        getframe;  
+        hold off
+%         set(get(AX(1),'xlim'),[times(start_index+1)-5, times(end_index-1)+5]);
+%         set(get(AX(2),'xlim'),[times(start_index+1)-5, times(end_index-1)+5]);
+%         set(get(AX(1),'Ylabel'),'String','Load') 
+%         set(get(AX(2),'Ylabel'),'String','Temperature') 
+        getframe;  
+        save2pdf ('load_pred.pdf', h, 600);
+        h = figure;
+        plot (all_times + date_offset, all_temps(:,best_i), 'k-', 'LineWidth', 2);
+        hold on;
+        plot ([min(test_times) + date_offset, min(test_times) + date_offset], [1, -2.5], 'k--');
+        plot ([max(test_times) + date_offset, max(test_times) + date_offset], [1, -2.5], 'k--');
+        xlim ([times(start_index+1)-5 + date_offset, times(end_index-1)+5 + date_offset]);
+        %ylim ([1, -2.5]);
+        xlabel('Time')
+        ylabel('Temperature (T06 standardised)')
+        datetick('x', 'dd mmm yyyy', 'keeplimits');
+        getframe;  
+        hold off;
+        save2pdf ('best_temp.pdf', h, 600);
+
+        % Save
+
+        loads(test_indices, i) = av_prediction(1:length(test_indices));
+        
+        % Wait so we can see what's happening
+        pause (1);
+        break;
+      end
+    elseif train(j)
+      searching_for_start = true;
+    end
+  end
+end
---
+++ source/temp/figures.m
@@ -0,0 +1,123 @@
+%% Produces figures
+
+%% Import data
+
+data = importdata('time_series_raw.csv',',');
+times = data(:,1);
+temps = data(:,2:end);
+clear data
+
+%% Split data into testing and training
+
+train = 1:39414;
+test  = 39415:39600;
+train = train';
+test = test';
+
+%% Remove means
+
+means = mean(temps(train,:));
+temps = temps - repmat(means, size(temps, 1), 1);
+
+%% Scale the data
+
+sds = std(temps(train,:));
+temps = temps ./ repmat(sds, size(temps, 1), 1);
+
+%% For plotting
+
+date_offset = 695421;
+
+%% Predict based on past + GP (independent models)
+
+for i = 1:1
+  % How far to look back in time? Obtained by counting rows in spreadsheet
+  one_year = 8784;
+  two_years = 17544;
+  three_years = 26304;
+  four_years = 35064;
+  % Construct the testing data
+  window = 1000;
+  train_indices = train((end-window+1):end);
+  all_indices = [train_indices; test];
+  train_times = times(train_indices);
+  all_times = times(all_indices);
+  train_temps = temps(train_indices, i);
+  historical_temps = (temps(all_indices - one_year, i) + ...
+                     temps(all_indices - two_years, i) + ...
+                     temps(all_indices - three_years, i) + ...
+                     temps(all_indices - four_years, i)) / 4;
+  % Smooth the data - local linear regression with bandwidth of one day
+  smoother = ksrlin (train_times, train_temps, 1, length(train_times));
+  train_smooth = smoother.f';
+  smoother = ksrlin (all_times, historical_temps, 1, length(all_times));
+  historical_smooth = smoother.f';
+  % Predict the smooth bit
+  x = train_times;
+  x_test = all_times;
+  y = train_smooth - historical_smooth(1:length(train_times));
+  % Setup the GP, train and predict
+  covfunc = {@covSEiso};
+  hyp.cov = [log(03) ; log(0.2)];
+  likfunc = @likGauss;
+  hyp.lik = log(0.05);
+  meanfunc = @meanZero;
+  hyp.mean = [];
+%   hyp_opt = minimize(hyp, @gp, -100, @infExact, meanfunc, covfunc, likfunc, x, y);
+  hyp_opt = hyp;
+                           
+  exp(hyp_opt.cov)
+  exp(hyp_opt.lik)
+          
+  [smooth_difference, ~] = gp(hyp_opt, @infExact, meanfunc, covfunc, likfunc, x, y, x_test);
+  % Now predict the periodic bit
+  % Construct the testing data
+  window = 250; % More recent than the trend - want to capture current variation
+  train_indices = train((end-window+1):end);
+  all_indices = [train_indices; test];
+  train_times = times(train_indices);
+  all_times = times(all_indices);
+  train_temps = temps(train_indices, i);
+  all_temps = temps(all_indices, i);
+  % Setup GP data
+  x = train_times;
+  x_test = all_times;
+  y = train_temps - train_smooth((end-window+1):end);
+  % Setup the GP, train and predict
+  covfunc = {@covPeriodic};
+  hyp.cov = [log(0.1) ; log(1) ; log(0.5)];
+  likfunc = @likGauss;
+  hyp.lik = log(0.15);
+  meanfunc = @meanZero;
+  hyp.mean = [];
+  hyp_opt = minimize(hyp, @gp, -100, @infExact, meanfunc, covfunc, likfunc, x, y);
+                           
+  exp(hyp_opt.cov)
+  exp(hyp_opt.lik)
+          
+  [m, ~] = gp(hyp_opt, @infExact, meanfunc, covfunc, likfunc, x, y, x_test);
+  prediction = m + historical_smooth((end-window-length(test)+1):end) + ...
+                   smooth_difference((end-window-length(test)+1):end);
+  % Plot
+  
+  h = figure;
+  plot (train_times + date_offset, train_temps, 'k-', 'LineWidth', 1);
+  hold on
+  plot (x_test + date_offset, historical_smooth((end-window-length(test)+1):end), 'c--', 'LineWidth', 6);
+  plot (x_test + date_offset, historical_smooth((end-window-length(test)+1):end) + ...
+                smooth_difference((end-window-length(test)+1):end), 'g:', 'LineWidth', 6);
+  plot (x_test + date_offset, prediction, 'r--', 'LineWidth', 2);
+  xlim ([max(train_times) - 10 + date_offset, max(times) + date_offset]);
+  xlabel('Time');
+  ylabel('Temperature (T01 standardised)');
+  datetick('x', 'dd mmm yyyy', 'keepticks');
+  hold off
+  getframe;
+  save2pdf ('temp_pred.pdf', h, 600);
+  
+  % Save prediction
+  temps(test, i) = prediction((end-length(test)+1):end);
+  
+  % Wait a bit
+  pause(1);
+end
